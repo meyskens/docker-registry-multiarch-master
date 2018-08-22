86a9380180d97236b7bf3cbfcef3757d804f86ce
Maartje Eyskens <meyskens@me.com>
2018-08-22 19:09:40 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+ARG arch
+FROM multiarch/debian-debootstrap:${arch}-stretch
+
+RUN apt-get update && apt-get -y upgrade
+RUN apt-get -y install git wget tar build-essential
+
+ARG arch
+ARG go_arch
+ARG checkout
+ARG go_version=1.9
+
+#Install golang
+RUN wget -O -  "https://golang.org/dl/go${go_version}.linux-${go_arch}.tar.gz" | tar xzC /usr/local
+ENV GOPATH /go
+ENV PATH $PATH:/usr/local/go/bin:$GOPATH/bin
+
+# Install registry
+RUN git clone https://github.com/docker/distribution.git -b ${checkout} /go/src/github.com/docker/distribution &&\
+    cd /go/src/github.com/docker/distribution/ &&\
+    go get &&\
+    make PREFIX=/go clean binaries &&\
+    mkdir -p /etc/docker/registry &&\
+    cp cmd/registry/config-dev.yml /etc/docker/registry/config.yml &&\
+    cd / && rm -fr /go/src
+
+EXPOSE 5000
+ENTRYPOINT ["registry"]
+CMD ["serve", "/etc/docker/registry/config.yml"]
---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+for i in "$@"
+    do
+        case $i in
+        --arch=*)
+            ARCH="${i#*=}"
+        ;;
+        --qemuarch=*)
+            QEMU_ARCH="${i#*=}"
+        ;;
+        --goach=*)
+            GO_ATCH="${i#*=}"
+        ;;
+        --qemuversion=*)
+            QEMU_VER="${i#*=}"
+        ;;
+        --repo=*)
+            DOCKER_REPO="${i#*=}"
+        ;;
+        --version=*)
+            VERSION="${i#*=}"
+        ;;
+        --checkout=*)
+            CHECKOUT="${i#*=}"
+        ;;
+        *)
+        # unknown option
+        ;;
+    esac
+done
+
+# install qemu-user-static
+if [ -n "${QEMU_ARCH}" ]; then
+    if [ ! -f x86_64_qemu-${QEMU_ARCH}-static.tar.gz ]; then
+        wget -N https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VER}/x86_64_qemu-${QEMU_ARCH}-static.tar.gz
+    fi
+    tar -xvf x86_64_qemu-${QEMU_ARCH}-static.tar.gz -C $ROOTFS/usr/bin/
+fi
+
+docker build --build-arg arch="${ARCH}" --build-arg go_arch="${GO_ARCH}"  --build-arg registry_version="${VERSION}" --build_arg checkout="${CHECKOUT}" -t "${DOCKER_REPO}:${ARCH}-${VERSION}" ./
